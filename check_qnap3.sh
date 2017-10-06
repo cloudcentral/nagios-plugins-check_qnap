beca0e4d339fcdda41ca79f4d00e0f37628d645f
martinko <gamato@users.sf.net>
2017-10-06 11:15:13 +0200

    Improve command 'temp'
    - awk(1) and cut(1) replaced with sed(1)
    - now it's more flexible and can process any number (more than 2 digits)
    - code cleaned up
    - typos fixed
    
    NOTE: It's not clear to me why there is upper limit on 80/89 degrees Celsius and why special message is output instead of standard CRITICAL message.

--- check_qnap3.sh
+++ check_qnap3.sh
@@ -147,10 +147,10 @@ elif [ "$strpart" == "cpu" ]; then
 # CPUTEMP ----------------------------------------------------------------------------------------------------------------------------------------------
 elif [ "$strpart" == "cputemp" ]; then
 	TEMP0="$(_snmpgetval .1.3.6.1.4.1.24681.1.2.5.0 | sed -E 's/"([0-9.]+) ?C.*/\1/')"
-	OUTPUT="CPU Temperature=${TEMP0}C|NAS CPUtermperature=${TEMP0}C;$strWarning;$strCritical;0;90"
+	OUTPUT="CPU Temperature=${TEMP0}C|NAS CPUtemperature=${TEMP0}C;$strWarning;$strCritical;0;90"
 
 	if [ "$TEMP0" -ge 89 ]; then
-		echo "CPU temperature too high!: "$OUTPUT
+		echo "CPU temperature too high!: $OUTPUT"
 		exit 3
 	else
 		if [ "$TEMP0" -ge "$strCritical" ]; then
@@ -191,25 +191,24 @@ elif [ "$strpart" == "freeram" ]; then
 
 # System Temperature---------------------------------------------------------------------------------------------------------------------------------------
 elif [ "$strpart" == "temp" ]; then
-    	TEMP0=$(_snmpget 1.3.6.1.4.1.24681.1.2.6.0 | awk '{print $4}' | cut -c2-3)
-	OUTPUT="Temperature="$TEMP0"C|NAS termperature="$TEMP0"C;$strWarning;$strCritical;0;80"
+	TEMP0="$(_snmpgetval .1.3.6.1.4.1.24681.1.2.6.0 | sed -E 's/"([0-9.]+) ?C.*/\1/')"
+	OUTPUT="Temperature=${TEMP0}C|NAS temperature=${TEMP0}C;$strWarning;$strCritical;0;80"
 
-    	if [ "$TEMP0" -ge "89" ]; then
-            	echo "System temperatur to high!: "$OUTPUT
-            	exit 2
-    	else
-
-            	if [ $TEMP0 -ge "$strCritical" ]; then
-                    	echo "CRITICAL: "$OUTPUT
-                    	exit 2
-            	fi
-            	if [ $TEMP0 -ge "$strWarning" ]; then
-                    	echo "WARNING: "$OUTPUT
-                    	exit 1
-            	fi
-            	echo "OK: "$OUTPUT
-            	exit 0
-    	fi
+	if [ "$TEMP0" -ge 89 ]; then
+		echo "System temperature too high!: $OUTPUT"
+		exit 3
+	else
+		if [ "$TEMP0" -ge "$strCritical" ]; then
+			echo "CRITICAL: $OUTPUT"
+			exit 2
+		fi
+		if [ "$TEMP0" -ge "$strWarning" ]; then
+			echo "WARNING: $OUTPUT"
+			exit 1
+		fi
+		echo "OK: $OUTPUT"
+		exit 0
+	fi
 
 # HD1 Temperature---------------------------------------------------------------------------------------------------------------------------------------
 elif [ "$strpart" == "hd1temp" ]; then
